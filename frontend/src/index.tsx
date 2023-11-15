590cdf25e2959273c264a52872d46428321ba10e
BriaN890501 <45000802+BriaN890501@users.noreply.github.com>
2023-11-15 19:15:33 +0800

    feat(frontend): login page
    login page using microsoft entra ID (default appearance)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+import { PublicClientApplication, EventType } from '@azure/msal-browser'
+import { msalConfig } from './authConfig'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/index.css'
+
+/**
+ * MSAL should be instantiated outside of the component tree to prevent it from being re-instantiated on re-renders.
+ * For more, visit: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/getting-started.md
+ */
+export const msalInstance: any = new PublicClientApplication(msalConfig)
+
+// Default to using the first account if no account is active on page load
+if (msalInstance.getActiveAccount() === null && msalInstance.getAllAccounts().length > 0) {
+  // Account selection logic is app dependent. Adjust as needed for different use cases.
+  msalInstance.setActiveAccount(msalInstance.getActiveAccount()[0])
+}
+
+// Listen for sign-in event and set active account
+msalInstance.addEventCallback((event: any) => {
+  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account != null) {
+    const account = event.payload.account
+    msalInstance.setActiveAccount(account)
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 root.render(
   <React.StrictMode>
-    <App />
+    <App instance={msalInstance}/>
   </React.StrictMode>
 )
 
