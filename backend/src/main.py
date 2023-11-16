3db6c3307457c24c6e0dee9da6874861b7176ea5
rainlonelyhuang <gino040615@google.com>
2023-11-16 13:49:12 +0800

    Add parking module

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,10 +2,12 @@
 from fastapi import FastAPI
 from src.users.router import router as user_router
 from src.spaces.router import router as spaces_router
+from src.parking.router import router as parking_router
 
 app = FastAPI()
 app.include_router(user_router, prefix="/api")
 app.include_router(spaces_router, prefix='/api')
+app.include_router(parking_router, prefix="/api")
 
 
 @app.get("/")
