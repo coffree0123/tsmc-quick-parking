d475e6793b375c30d1a4623afd3f9fdc98dc818a
coffree <02320013apis@gmail.com>
2023-11-17 20:10:47 +0800

    feat(backend): user info api - get_user_vehicles && get_parkinglot version

--- backend/src/users/router.py
+++ backend/src/users/router.py
@@ -21,3 +21,13 @@ def add_vehicle(r: Request, user_id: int, license_id: str, nick_name: str,
                 car_size: VehicleSize = "small") -> None:
     '''Add a new vehicle to the database'''
     r.app.state.database.add_vehicle(user_id, license_id, nick_name, car_size)
+
+
+@router.get("/users/user_info/{user_id}")
+def get_user_info(r: Request, user_id: int) -> dict[str, list[str]]:
+    '''Get user information'''
+    license_list, nickname_list = r.app.state.database.get_user_vehicles(
+        user_id)
+    parkinglot_list = [r.app.state.database.get_parkinglot(
+        license_id) for license_id in license_list]
+    return {"parked_vehicles": nickname_list, "parking_lots": parkinglot_list}
