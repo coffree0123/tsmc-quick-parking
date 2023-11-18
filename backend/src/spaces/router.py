8adbf729269572dac47b72cb14485be62bb0adf8
Buster <apple8952047@gmail.com>
2023-11-18 21:01:30 +0800

    prototype for general lastest record searching

--- backend/src/spaces/router.py
+++ backend/src/spaces/router.py
@@ -1,5 +1,5 @@
 '''Parking Space Management Module'''
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException, status
 
 router = APIRouter()
 
@@ -10,7 +10,14 @@ def read_free_spaces(r: Request, parkinglot_id: int) -> list[tuple[int, int]]:
     return r.app.state.database.get_free_spaces(parkinglot_id)
 
 
-@router.get(path="/spaces/vehicle")
-def get_latest_records(r: Request, vehicle_id: str) -> list[dict]:
+@router.get(path="/spaces/records")
+def get_latest_records(r: Request, vehicle_id: str = None, user_id: int = None) -> list[dict]:
     '''Get a list of parking records of a vehicle'''
-    return r.app.state.database.get_vehicle_latest_records(vehicle_id)
+    if vehicle_id is None and user_id is None:
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST, 
+            detail="At least one of user_id and vehicle_id must be provided"
+        )
+    if vehicle_id is not None:
+        return r.app.state.database.get_vehicle_latest_records(vehicle_id)
+    return []
