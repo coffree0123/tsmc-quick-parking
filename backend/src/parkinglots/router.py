37d958e9608c89742a69e66060865626a912722e
Buster <apple8952047@gmail.com>
2023-11-21 00:25:40 +0800

    refactor(backend): rename spaces module to parkinglots

--- backend/src/parkinglots/router.py
+++ backend/src/parkinglots/router.py
@@ -4,13 +4,20 @@ from fastapi import APIRouter, Request, HTTPException, status
 router = APIRouter()
 
 
-@router.get(path="/spaces/free")
+@router.get(path="/parkinglots/{parkinglot_id}/free-slots")
 def read_free_spaces(r: Request, parkinglot_id: int) -> list[tuple[int, int]]:
     '''Returns a list of free spaces of a parking lot'''
     return r.app.state.database.get_free_spaces(parkinglot_id)
 
 
-@router.get(path="/spaces/records")
+@router.get(path="/parkinglots/{parkinglot_id}/long-term-occupants")
+def get_long_term_occupants(r: Request, parkinglot_id: int) -> list[dict]:
+    '''Search the vehicles that park the longest in a parking lot'''
+    return r.app.state.database.get_long_term_occupants(parkinglot_id)
+
+
+# extend the api
+@router.get(path="/parkinglots/records")
 def get_latest_records(r: Request, vehicle_id: str = None, user_id: int = None) -> list[dict]:
     '''Search the parking records by user id or vehicle id'''
     if vehicle_id is None and user_id is None:
@@ -21,7 +28,4 @@ def get_latest_records(r: Request, vehicle_id: str = None, user_id: int = None) 
     return r.app.state.database.get_latest_records(vehicle_id, user_id)
 
 
-@router.get(path="/spaces/long-term")
-def get_long_term_occupants(r: Request, parkinglot_id: int) -> list[dict]:
-    '''Search the vehicles that park the longest in a parking lot'''
-    return r.app.state.database.get_long_term_occupants(parkinglot_id)
+
