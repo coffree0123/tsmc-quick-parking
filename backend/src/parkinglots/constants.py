ef5a28f40ae7eb31b80b6050e9c4d10fd6d1da4a
Buster <apple8952047@gmail.com>
2023-11-21 18:08:15 +0800

    resolve pylint complain

--- backend/src/parkinglots/constants.py
+++ backend/src/parkinglots/constants.py
@@ -1,7 +1,9 @@
+'''Parking Lot module constants'''
 from pydantic import BaseModel
 
 
 class FreeSpace(BaseModel):
+    '''Return class of free space'''
     num_row: int = 0
     num_col: int = 0
     num_floor: int = 0
