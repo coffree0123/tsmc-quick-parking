03b8c0d628ba14e003d9d5037eff938cf7238cdc
coffree <02320013apis@gmail.com>
2023-11-22 17:17:20 +0800

    feat(backend): update vehicle delete api

--- backend/src/vehicles/router.py
+++ backend/src/vehicles/router.py
@@ -9,3 +9,8 @@ def add_vehicle(r: Request, user_id: int, license_id: str, nick_name: str,
                 car_size: VehicleSize = "small") -> None:
     '''Add a new vehicle to the database'''
     r.app.state.database.add_vehicle(user_id, license_id, nick_name, car_size)
+
+@router.delete("/vehicles/")
+def delete_vehicle(r: Request, license_id: str) -> None:
+    '''Add a new vehicle to the database'''
+    r.app.state.database.delete_vehicle(license_id)
