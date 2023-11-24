e78c0bf20574858ce856f33323d4ac9cdfb2b196
Yu-Hsiang Huang <02320013apis@gmail.com>
2023-11-24 16:30:15 +0800

    feat(backend): Add unit test example (#52)
    * feat(backend): Add unit test example
    
    * refactor(backend): mock db as fixture
    
    * devops(backend): update github action with unit test
    
    * devops(backend): update github action backend name
    
    * fix(backend): pytest error
    
    * fix(devops): update precommit for backend pylint

---
+++ backend/tests/users/test_router.py
@@ -0,0 +1,51 @@
+'''Test user router'''
+from unittest.mock import Mock
+from fastapi.testclient import TestClient
+import pytest
+
+from src.main import app
+
+
+@pytest.fixture()
+def user_data():
+    '''User data for testing'''
+    return {
+        "first_name": "John",
+        "last_name": "Doe",
+        "email": "john.doe@example.com",
+        "phone_num": "1234567890",
+        "gender": "male",
+        "age": 30,
+        "job_title": "engineer",
+        "special_role": ""
+    }
+
+
+@pytest.fixture()
+def mock_db():
+    '''Mock database'''
+    fake_db = Mock()
+    fake_db.add_user.return_value = 1
+    fake_db.update_user.return_value = None
+    return fake_db
+
+
+def test_create_user(mocker, mock_db, user_data):
+    '''Test create user'''
+    # mock database
+    mocker.patch("src.main.QuickParkingDB", return_value=mock_db)
+
+    with TestClient(app) as client:
+        response = client.post("/api/users", json=user_data)
+        assert response.status_code == 200
+        assert "user_id" in response.json()
+
+
+def test_update_user(mocker, mock_db, user_data):
+    '''Test update user'''
+    # mock database
+    mocker.patch("src.main.QuickParkingDB", return_value=mock_db)
+
+    with TestClient(app) as client:
+        response = client.put("/api/users/1", json=user_data)
+        assert response.status_code == 200
