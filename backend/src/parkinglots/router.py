7b5aed5ffac2a9dc02566c667e4a7c065caee5ad
Buster <apple8952047@gmail.com>
2023-11-28 20:50:13 +0800

    feat(backend): parking lot api

--- backend/src/parkinglots/router.py
+++ backend/src/parkinglots/router.py
@@ -1,34 +1,42 @@
 '''Parking Space Management Module'''
 from collections import defaultdict
-from fastapi import APIRouter, Request
-from src.constants import FreeSpace
+from fastapi import APIRouter, Request, HTTPException, status
+from src.constants import ParkingLot, FloorInfo
 
 
 router = APIRouter()
 
 
-@router.get(path="/parkinglots/{parkinglot_id}/free-slots")
-def get_free_spaces(r: Request, parkinglot_id: int) -> FreeSpace:
+# user api
+@router.get(path="/parkinglots/{parkinglot_id}")
+def get_parkinglot(r: Request, parkinglot_id: int) -> ParkingLot:
     '''Returns a list of free spaces of a parking lot'''
     # parking lot info
     parkinglot_info = r.app.state.database.get_parkinglot_info(parkinglot_id)
     if not parkinglot_info:
-        return FreeSpace()
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail=f"Parking lot of id {parkinglot_id} doesn't exist"
+        )
     parkinglot_info = parkinglot_info[0]
 
-    # get free slots in the parking lot
+    # get floor information (currently only free slots)
     free_slots = defaultdict(list)
     for floor, idx in sorted(r.app.state.database.get_free_spaces(parkinglot_id)):
-        free_slots['B' + str(floor)].append(idx)
+        free_slots[floor].append(idx)
+    floor_info = [
+        FloorInfo(floor=f"B{k}", free_slots=v) for k, v in sorted(free_slots.items())
+    ]
 
-    return FreeSpace(
+    return ParkingLot(
         num_row=parkinglot_info["numRow"],
         num_col=parkinglot_info["numCol"],
         num_floor=parkinglot_info["numFloor"],
-        free_slots=free_slots,
+        floor_info=floor_info,
     )
 
 
+# guard api
 @router.get(path="/parkinglots/{parkinglot_id}/long-term-occupants")
 def get_long_term_occupants(r: Request, parkinglot_id: int) -> list[dict]:
     '''Search the vehicles that park the longest in a parking lot'''
