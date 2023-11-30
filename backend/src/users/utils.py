6170946e374ea5cff1e4926453132d8618c8742d
coffree <02320013apis@gmail.com>
2023-11-30 10:49:20 +0800

    fix(backend): favorite parking lot example

--- backend/src/users/utils.py
+++ backend/src/users/utils.py
@@ -5,6 +5,6 @@ def get_user_favorite_parkinglot() -> list[BuildingInfo]:
     '''Get user's favorite parking lot'''
     # This function will be updated later
     return [
-        BuildingInfo(0, "Building A", 30),
-        BuildingInfo(1, "Factory B", 80)
+        BuildingInfo(1, "Building A", 30),
+        BuildingInfo(2, "Factory B", 80)
     ]
