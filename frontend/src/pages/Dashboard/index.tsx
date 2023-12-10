ca0a7aad688fae41e153b50542126dc6941e3939
rainlonelyhuang <48359695+rainlonelyhuang@users.noreply.github.com>
2023-12-10 14:39:49 +0800

    Feat frontend guard dashboard (#101)
    * feat(frontend): long-term occupants
    
    * feat(backend): time-records output list
    
    * feat(frontend): add guard dashboard chart
    
    ---------
    
    Co-authored-by: JackywithaWhiteDog <jackyliu0129@gmail.com>

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,37 +1,156 @@
-import React from 'react'
-import { Col, Input, Layout, List, Row, Space, Typography } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Col, Input, Layout, List, Row, Space, Typography, Select, DatePicker } from 'antd'
 import { SearchOutlined } from '@ant-design/icons'
-import ParkingLot from '../../components/ParkingLot'
+import { Line } from '@ant-design/charts'
+// import ParkingLot from '../../components/ParkingLot'
 import LogOutButton from '../../components/LogOutButton'
 import { MsalProvider } from '@azure/msal-react'
+import axios from 'axios'
+import dayjs from 'dayjs'
 
 const { Header, Content } = Layout
 const { Title } = Typography
+const { RangePicker } = DatePicker
 
 interface VehicleProps {
   license: string
-  slot: string
-  stayTime: string
+  position: string
+  stayTime: number
 }
 
 const Vehicle = (props: VehicleProps): React.ReactElement => {
+  const days = Math.floor(props.stayTime / (1000 * 60 * 60 * 24))
+  const hours = Math.floor(props.stayTime / (1000 * 60 * 60) - days * 24)
+  const minutes = Math.round(props.stayTime / (1000 * 60) - (days * 24 + hours) * 60)
   return (
     <Row style={{ width: '100%' }}>
       <Col span={8}>{props.license}</Col>
-      <Col span={8}>{props.slot}</Col>
-      <Col span={8} style={{ textAlign: 'end' }}>{props.stayTime}</Col>
+      <Col span={8}>{props.position}</Col>
+      <Col span={8} style={{ textAlign: 'end' }}>{days > 0 ? `${days} day${days > 1 ? 's' : ''} ` : ''}{hours > 0 ? `${hours} hr${hours > 1 ? 's' : ''} ` : ''}{minutes > 0 ? `${minutes} min${minutes > 1 ? 's' : ''}` : ''}</Col>
     </Row>
   )
 }
 
-const Dashboard = ({ instance }: any): React.ReactElement => {
-  const vehicles: VehicleProps[] = [
-    { license: 'ABC-9988', slot: 'B1#105', stayTime: '7 days 18 hours' },
-    { license: 'EAH-1677', slot: 'B2#222', stayTime: '1 days 3 hours' },
-    { license: 'JDU-9132', slot: 'B2#213', stayTime: '14 hours' },
-    { license: 'AKS-5723', slot: 'B3#385', stayTime: '4 hours' }
-  ]
+interface OccupantInfo {
+  position: string
+  license: string
+  startTime: number
+}
 
+interface OccupantRes {
+  postion: string
+  license_plate_no: string
+  start_time: string
+}
+
+interface TimeRecordInfo {
+  time: string
+  value: number
+}
+
+const Occupants = (props: { id: number }): React.ReactElement => {
+  const [occupants, setOccupants] = useState<OccupantInfo[]>([])
+
+  useEffect(() => {
+    axios.get<OccupantRes[]>(`guards/parkinglots/${props.id}/long-term-occupants`)
+      .then(response => {
+        setOccupants(response.data.map(item => ({
+          position: item.postion,
+          license: item.license_plate_no,
+          startTime: Date.parse(item.start_time)
+        })))
+      })
+      .catch(error => { console.error(error) })
+  }, [])
+
+  return (
+    <List
+      size='large'
+      bordered
+      dataSource={occupants}
+      renderItem={(item) => (
+        <List.Item>
+          <Vehicle license={item.license} position={item.position} stayTime={Date.now() - item.startTime} />
+        </List.Item>
+      )}
+    />
+  )
+}
+const Chart = (): React.ReactElement => {
+  const [timeRecords, setTimeRecords] = useState<TimeRecordInfo[]>([])
+  const [parkingLotID, setParkingLotID] = useState<string>('1')
+  const [floor, setFloor] = useState<string>('1')
+  const [timeRange, setTimeRange] = useState<string[]>([dayjs().subtract(1, 'day').toString(), dayjs().toString()])
+  // const [interval, setInterval] = useState<string>('1H')
+
+  useEffect(() => {
+    axios.get<TimeRecordInfo[]>(`guards/dashboard/time-records?parkinglot_id=${parkingLotID}&floor=${floor}&start_time=${timeRange[0]}&end_time=${timeRange[1]}&interval=1H`)
+      .then(response => {
+        setTimeRecords(response.data.map(item => ({
+          time: item.time,
+          value: item.value
+        })))
+      })
+      .catch(error => { console.error(error) })
+  }, [parkingLotID, floor, timeRange])
+
+  const config = {
+    data: timeRecords,
+    width: 800,
+    height: 400,
+    autoFit: false,
+    xField: 'time',
+    yField: 'value',
+    point: {
+      size: 5,
+      shape: 'diamond'
+    },
+    label: {
+      style: {
+        fill: '#aaa'
+      }
+    }
+  }
+  return (
+    <React.Fragment>
+      <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+        <Space wrap>
+          Pakring Lot
+          <Select
+            defaultValue="1"
+            style={{ width: 120 }}
+            options={[
+              { value: '1', label: '1' },
+              { value: '2', label: '2' }
+            ]}
+            onChange={(val) => { setParkingLotID(val) }}
+          />
+          Floor
+          <Select
+            defaultValue="1"
+            style={{ width: 120 }}
+            options={[
+              { value: '1', label: '1' },
+              { value: '2', label: '2' }
+            ]}
+            onChange={(val) => { setFloor(val) }}
+          />
+        </Space>
+        <Space wrap>
+          <RangePicker
+            showTime={{ format: 'HH:mm' }}
+            format="YYYY-MM-DD HH:mm"
+            defaultValue={[dayjs().subtract(1, 'day'), dayjs()]}
+            onChange={(time, timeString) => { setTimeRange(timeString) }}
+          />
+        </Space>
+        <Line {...config} />
+      </Space>
+    </React.Fragment>
+  )
+}
+
+const Dashboard = ({ instance }: any): React.ReactElement => {
   return (
     <MsalProvider instance={instance}>
     <Layout>
@@ -59,22 +178,18 @@ const Dashboard = ({ instance }: any): React.ReactElement => {
         }}
       >
         <Row>
-          <Col span={12}>
-            <Title level={3}>Long-term occupancy</Title>
-            <List
-              size='large'
-              bordered
-              dataSource={vehicles}
-              renderItem={(item) => (
-                <List.Item>
-                  <Vehicle license={item.license} slot={item.slot} stayTime={item.stayTime} />
-                </List.Item>
-              )}
-            />
+          <Col span={18} offset={6}>
+            <Chart />
           </Col>
+        </Row>
+        <Row>
           <Col span={12}>
+            <Title level={3}>Long-term occupants</Title>
+            <Occupants id={1} />
+          </Col>
+          {/* <Col span={12}>
             <ParkingLot id={1} />
-          </Col>
+          </Col> */}
         </Row>
       </Content>
     </Layout>
