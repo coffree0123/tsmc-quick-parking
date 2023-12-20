88d5062bc60a55a87803b8ea93a000a2b4794dfc
rainlonelyhuang <48359695+rainlonelyhuang@users.noreply.github.com>
2023-12-20 14:53:41 +0800

    Fix backend record timezone (#170)
    * fix(backend): add timezone in parking record
    
    * fix(backend): add timezone in dashboard router
    
    * fix(frontend): add timezone in time record query
    
    * fix(frontend): add timezone in RangePicker
    
    * fix(backend): timezone confliction
    
    * feat(frontend): show current parking lot
    
    ---------
    
    Co-authored-by: JackywithaWhiteDog <jackyliu0129@gmail.com>

--- backend/clear_records.sh
+++ backend/clear_records.sh
@@ -1,5 +1,5 @@
 # This script clears all rows of ParkingRecords in the database with the clear_record.sql file.
 
-docker cp docker/db/clear_records.sql park:/tmp/clear_records.sql &&
-docker exec park bash -c "psql < /tmp/clear_records.sql"
+docker cp docker/db/clear_records.sql quick-parking-db:/tmp/clear_records.sql &&
+docker exec quick-parking-db bash -c "psql -U quickparking < /tmp/clear_records.sql"
 
